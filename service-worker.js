1cb68085e900589895061b95563640282e8cc239
lobsterbandit <code.lobsterbandit@outlook.com>
2020-04-04 12:26:29 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/use-countdown-timer/precache-manifest.31d21df2503a19108067ef7f0c7aa5af.js"
+  "/use-countdown-timer/precache-manifest.14dc6ffb7443f403188d5d78b6056305.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/use-countdown-timer/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
