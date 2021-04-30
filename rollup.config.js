7b9e130ea3cdd12e4280bb2a703275fe18cab6e3
lobsterbandit <code.lobsterbandit@outlook.com>
2021-04-30 15:59:11 -0400

    Fix .d.ts generation

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,54 @@
+import path from 'path'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-
 import pkg from './package.json'
 
-const tsconfigOverride = { exclude: ['src/**/*.test.ts'] }
+const input = 'src/index.ts'
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+]
 
-export default {
-  input: 'src/index.ts',
-  external: ['react'],
-  output: [
-    {
-      file: pkg.main,
+const getPlugins = (format = 'esm') => {
+  const typeScriptOptions =
+    format === 'esm'
+      ? { declaration: true, declarationDir: path.dirname(pkg.module) }
+      : {}
+
+  return [resolve(), typescript(typeScriptOptions), commonjs()]
+}
+
+export default [
+  {
+    input,
+    output: {
+      // file: pkg.main,
+      dir: path.dirname(pkg.main),
       format: 'cjs',
       exports: 'named',
       sourcemap: true,
     },
-    {
+    plugins: [
+      resolve(),
+      typescript({
+        declaration: true,
+        declarationDir: path.dirname(pkg.module),
+        exclude: ['src/**/*.test.ts'],
+      }),
+      commonjs(),
+    ],
+    external,
+  },
+  {
+    input,
+    output: {
       file: pkg.module,
-      format: 'es',
+      format: 'esm',
       exports: 'named',
       sourcemap: true,
     },
-  ],
-  plugins: [resolve(), typescript(tsconfigOverride), commonjs()],
-}
+    plugins: [resolve(), typescript(), commonjs()],
+    external,
+  },
+]
